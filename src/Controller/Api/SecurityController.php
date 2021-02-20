586bcd64978ae45e00ddabf95b8c8696e2c5e3a1
tuc0w <andreasbehrend@gmail.com>
2021-02-20 23:39:07 +0100

    Add authentication route

--- src/Controller/Api/SecurityController.php
+++ src/Controller/Api/SecurityController.php
@@ -1,21 +1,23 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Controller\Api;
 
+use App\Dto\SignIn;
 use App\Entity\User;
 use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use FOS\RestBundle\Controller\Annotations as Rest;
+use Nelmio\ApiDocBundle\Annotation\Model;
 use OpenApi\Annotations as OA;
 use Psl\Type as Type;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
-/**
- * @OA\Tag(name="User")
- */
-class SecurityController
+class SecurityController extends AbstractController
 {
     private UserRepository $userRepository;
     private EntityManagerInterface $entityManager;
@@ -29,8 +31,38 @@ class SecurityController
     }
 
     /**
-     * @Route(path="/api/user/register", methods={"POST"})
+     * @Rest\Post("api/authentication/sign-in")
+     * 
+     * @OA\Tag(name="Authentication")
+     * 
+     * @OA\RequestBody(
+     *     description="SignIn",
+     *     required=true
+     * )
+     * 
+     * @OA\Response(
+     *     response=200,
+     *     description="If the authentification succeded, a JWT token is returned"
+     * )
+     * 
+     * @param Request $request
+     * 
+     */
+    public function signIn(Request $request)
+    {
+        $user = $this->getUser();
+
+        return $this->json([
+            'username' => $user->getUsername(),
+            'roles' => $user->getRoles(),
+        ]);
+    }
+
+    /**
+     * @Rest\Post("api/user/register")
      *
+     * @OA\Tag(name="User")
+     * 
      * @OA\Response(
      *     response="204",
      *     description="When the registration was successful"
