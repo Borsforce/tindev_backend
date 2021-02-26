7d1c4d98a97002340616844667c89d851f56d5f7
Yannick Fricke <yannickfricke@googlemail.com>
2021-02-26 19:18:38 +0100

    Added the CORS header

--- public/index.php
+++ public/index.php
@@ -18,5 +18,8 @@ if ($_SERVER['APP_DEBUG']) {
 $kernel = new Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
+
+header('Access-Control-Allow-Origin: *');
+
 $response->send();
 $kernel->terminate($request, $response);
