cde395d29d6ac019a5b1257634cc6ee9e5327dc1
tuc0w <andreasbehrend@gmail.com>
2021-03-08 23:51:28 +0100

    Remove allow origin header from index.php

--- public/index.php
+++ public/index.php
@@ -19,7 +19,5 @@ $kernel = new Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 
-header('Access-Control-Allow-Origin: *');
-
 $response->send();
 $kernel->terminate($request, $response);
